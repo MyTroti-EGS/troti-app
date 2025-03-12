4592d69f850efdbab08e204856ed77569e3c7349
Miguel Vila <miguelovila@ua.pt>
2025-03-12 05:02:07 +0000

    fix: notification bar gradient

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,20 @@
 import { Stack } from 'expo-router';
+import GradientStatusBar from '~/components/GradientStatusBar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
+      <GradientStatusBar
+        colors={[
+          'rgba(0,0,0,0.6)',
+          'rgba(0,0,0,0.5)',
+          'rgba(0,0,0,0.3)',
+          'rgba(0,0,0,0.2)',
+          'transparent',
+        ]}
+        barStyle="light"
+      />
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(tabs)" />
       </Stack>
