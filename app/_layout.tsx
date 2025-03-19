3308e391b9e44db54a43127d6901c795779309fe
Miguel Vila <miguelovila@ua.pt>
2025-03-19 02:01:27 +0000

    feat: integrate AuthProvider and request location permissions in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,46 @@
-import { Stack } from 'expo-router';
+import { Slot } from 'expo-router';
+import AuthProvider from 'providers/AuthProvider';
 import GradientStatusBar from '~/components/GradientStatusBar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import * as Location from 'expo-location';
+import { useEffect } from 'react';
+
 export default function RootLayout() {
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        alert('Location permission is required to use the app.');
+      }
+    })();
+  }, []);
+
   return (
-    <SafeAreaProvider>
-      <GradientStatusBar
-        colors={[
-          'rgba(0,0,0,0.6)',
-          'rgba(0,0,0,0.5)',
-          'rgba(0,0,0,0.3)',
-          'rgba(0,0,0,0.2)',
-          'transparent',
-        ]}
-        barStyle="light"
-      />
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-    </SafeAreaProvider>
+    <AuthProvider>
+      <SafeAreaProvider>
+        <GradientStatusBar
+          colors={[
+            'rgba(0,0,0,0.6)',
+            'rgba(0,0,0,0.5)',
+            'rgba(0,0,0,0.3)',
+            'rgba(0,0,0,0.2)',
+            'transparent',
+          ]}
+          barStyle="light"
+        />
+        <Slot />
+      </SafeAreaProvider>
+    </AuthProvider>
   );
 }
+
+/*
+
+<Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="auth/index" />
+        <Stack.Screen name="(main-tabs)" />
+      </Stack>
+
+*/
